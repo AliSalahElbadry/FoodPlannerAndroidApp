3588f94ec22c98fa1d9dcb184486f6f14f8a3315
marinaAbdelmalak2000 <83098969+marinaAbdelmalak2000@users.noreply.github.com>
2023-02-03 15:44:59 +0200

    interface classes

---
+++ app/src/main/java/com/app/our/foodplanner/app_vp/view/profile/ProfileFragmentInterface.java
@@ -0,0 +1,4 @@
+package com.app.our.foodplanner.app_vp.view.profile;
+
+public interface ProfileFragmentInterface {
+}
---
+++ app/src/main/java/com/app/our/foodplanner/app_vp/view/signup_login/Signup_Login_Fragment_Interface.java
@@ -0,0 +1,4 @@
+package com.app.our.foodplanner.app_vp.view.signup_login;
+
+public interface Signup_Login_Fragment_Interface {
+}
