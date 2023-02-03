915e4fc2e813901c5ba11109951f881764f05493
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-02-03 23:21:35 +0200

    animation of splash

---
+++ app/src/main/java/com/app/our/foodplanner/app_vp/view/MainActivityContainer.java
@@ -0,0 +1,16 @@
+package com.app.our.foodplanner.app_vp.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.app.our.foodplanner.R;
+
+public class MainActivityContainer extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
